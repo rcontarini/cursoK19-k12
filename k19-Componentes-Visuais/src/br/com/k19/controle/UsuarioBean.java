bd39faa2a7dc1988e588951aa389580ef4240a7b
rcontarini <contbeiro@gmail.com>
2016-01-27 08:49:55 -0200

    Componentes Visuais

---
+++ k19-Componentes-Visuais/src/br/com/k19/controle/UsuarioBean.java
@@ -0,0 +1,27 @@
+package br.com.k19.controle;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class UsuarioBean {
+
+	private String nome;
+	private int idade;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+}
